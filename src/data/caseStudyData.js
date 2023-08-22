7ca5846cdd4375e80275d2b8600218856d461f5f
Naman Mittal <mittalnaman08@gmail.com>
2023-08-23 00:03:18 +0530

    added case study

--- src/data/caseStudyData.js
+++ src/data/caseStudyData.js
@@ -216,63 +216,63 @@ const caseStudyData = [
 
     slug: "BizInsights",
   },
-  {
-    id: 4,
-    title: "Responsive Website Design for Metablock Technologies",
-    description: "As the lead front-end developer, I successfully designed and implemented a responsive corporate website for Metablock Technologies..",
+  // {
+  //   id: 4,
+  //   title: "Responsive Website Design for Metablock Technologies",
+  //   description: "As the lead front-end developer, I successfully designed and implemented a responsive corporate website for Metablock Technologies..",
     
-    tags: [
-      "Responsive Web Design",
-      "Front-End Development",
-      "Web Accessibility",
-    ],
+  //   tags: [
+  //     "Responsive Web Design",
+  //     "Front-End Development",
+  //     "Web Accessibility",
+  //   ],
 
-    imageUrl: "/images/metablock.png",
+  //   imageUrl: "/images/metablock.png",
 
 
     
-    overviewPara1: "Metablock Technologies, a startup offering blockchain consulting and development services, required a new corporate website to establish their brand and attract potential clients. As the lead front-end developer, I was responsible for implementing the responsive website design provided by the design team. The key requirements were to accurately translate the visual mockups into pixel-perfect web pages, while ensuring optimal user experience across devices. This involved leveraging modern web technologies including HTML5, CSS3, JavaScript ES6, jQuery and Git version control to build an accessible, high-performance, and SEO-friendly website aligned with Metablock's brand identity.",
+  //   overviewPara1: "Metablock Technologies, a startup offering blockchain consulting and development services, required a new corporate website to establish their brand and attract potential clients. As the lead front-end developer, I was responsible for implementing the responsive website design provided by the design team. The key requirements were to accurately translate the visual mockups into pixel-perfect web pages, while ensuring optimal user experience across devices. This involved leveraging modern web technologies including HTML5, CSS3, JavaScript ES6, jQuery and Git version control to build an accessible, high-performance, and SEO-friendly website aligned with Metablock's brand identity.",
     
-    overviewPara2: "Throughout the agile development process, I collaborated closely with stakeholders to iterate on the layouts, components, and features. Extensive testing was done to guarantee cross-browser compatibility, responsiveness on mobile devices, compliance with web standards, and accurate representation of the brand styleguide. The end result exceeded expectations in effectively communicating Metablock's capabilities and expertise through a visually stunning and interactive website.",
+  //   overviewPara2: "Throughout the agile development process, I collaborated closely with stakeholders to iterate on the layouts, components, and features. Extensive testing was done to guarantee cross-browser compatibility, responsiveness on mobile devices, compliance with web standards, and accurate representation of the brand styleguide. The end result exceeded expectations in effectively communicating Metablock's capabilities and expertise through a visually stunning and interactive website.",
 
-    process: "The projectfollowed an agile methodology with 2-week sprints. Requirements were gathered through meetings with stakeholders. Design mockups were provided by the design team. I implemented the frontend using HTML, CSS, Javascript, and jQuery, meeting accessibility and performance standards. Version control was maintained in Git. Extensive testing ensured cross-browser compatibility.",
+  //   process: "The projectfollowed an agile methodology with 2-week sprints. Requirements were gathered through meetings with stakeholders. Design mockups were provided by the design team. I implemented the frontend using HTML, CSS, Javascript, and jQuery, meeting accessibility and performance standards. Version control was maintained in Git. Extensive testing ensured cross-browser compatibility.",
     
-    // resultHeading: "Result",
+  //   // resultHeading: "Result",
 
-    // resultPara: "The completed website effectively communicates Metablock's capabilities through a clean, modern design. Call-to-action buttons and contact forms generate leads. A news section and case studies establish thought leadership. Responsive design provides an optimal experience on mobile devices. The site aligned with brand guidelines and exceeded stakeholder expectations.",
+  //   // resultPara: "The completed website effectively communicates Metablock's capabilities through a clean, modern design. Call-to-action buttons and contact forms generate leads. A news section and case studies establish thought leadership. Responsive design provides an optimal experience on mobile devices. The site aligned with brand guidelines and exceeded stakeholder expectations.",
     
 
-    featuresPointHeading1: 'UI UX Management',
-    featuresPointHeading2: 'Generating Leads',
-    featuresPointHeading3: 'Worked as a Team',
-    featuresPointHeading4: 'Finished on time',
-    featuresPointHeading5: 'Internship Experience',
+  //   featuresPointHeading1: 'UI UX Management',
+  //   featuresPointHeading2: 'Generating Leads',
+  //   featuresPointHeading3: 'Worked as a Team',
+  //   featuresPointHeading4: 'Finished on time',
+  //   featuresPointHeading5: 'Internship Experience',
 
-    featuresPoint1: 'Conveyed expertise and built trust through content, visuals and UX',
-    featuresPoint2: 'Generated leads through clear calls-to-action and contact forms',
-    featuresPoint3: 'Ensured great user experience across devices with responsive design',
-    featuresPoint4: 'Delivered site on-time and within budget through agile process',
-    featuresPoint5: 'Got experience to work as a team During the 6 month internship',
+  //   featuresPoint1: 'Conveyed expertise and built trust through content, visuals and UX',
+  //   featuresPoint2: 'Generated leads through clear calls-to-action and contact forms',
+  //   featuresPoint3: 'Ensured great user experience across devices with responsive design',
+  //   featuresPoint4: 'Delivered site on-time and within budget through agile process',
+  //   featuresPoint5: 'Got experience to work as a team During the 6 month internship',
 
-    featuresHeading: 'Key Outcomes',
+  //   featuresHeading: 'Key Outcomes',
 
 
-    benifitsStyle: '{"display": "none"}',
-    benifitsHeading: '',
+  //   benifitsStyle: '{"display": "none"}',
+  //   benifitsHeading: '',
 
-    techStack: "HTML5, CSS, Javascript, JQuery, git",
+  //   techStack: "HTML5, CSS, Javascript, JQuery, git",
     
-    conclusionPara1: "The development of Metablock’s new corporate website was a success that aligned with their goals of building their brand, attracting clients, and generating business opportunities. As the lead front-end developer, I applied my skills in HTML5, CSS3, JavaScript, jQuery and responsive design to deliver a visually stunning, optimized, and fully-functional website. The pixel-perfect and accessible implementation along with robust performance resulted in an excellent user experience that conveyed Metablock’s blockchain expertise. Despite the tight timeline, the project was delivered on-schedule and within budget through agile practices and close collaboration with stakeholders. ",
+  //   conclusionPara1: "The development of Metablock’s new corporate website was a success that aligned with their goals of building their brand, attracting clients, and generating business opportunities. As the lead front-end developer, I applied my skills in HTML5, CSS3, JavaScript, jQuery and responsive design to deliver a visually stunning, optimized, and fully-functional website. The pixel-perfect and accessible implementation along with robust performance resulted in an excellent user experience that conveyed Metablock’s blockchain expertise. Despite the tight timeline, the project was delivered on-schedule and within budget through agile practices and close collaboration with stakeholders. ",
 
-    conclusionPara2: "The positive user feedback, increased traffic and higher lead conversion validated the quality of the end product. My technical skills and attention to detail were instrumental in building an impactful online presence for Metablock to establish thought leadership in their industry. This project provided invaluable experience and I am proud to have contributed to the company’s vision coming to life through their new website.",
+  //   conclusionPara2: "The positive user feedback, increased traffic and higher lead conversion validated the quality of the end product. My technical skills and attention to detail were instrumental in building an impactful online presence for Metablock to establish thought leadership in their industry. This project provided invaluable experience and I am proud to have contributed to the company’s vision coming to life through their new website.",
 
-    lastInfo: "Checkout the website: ",
+  //   lastInfo: "Checkout the website: ",
 
-    exploreLink: "https://metablocktechnologies.org/",
+  //   exploreLink: "https://metablocktechnologies.org/",
 
 
-    slug: "metablock-technologies"
-    },
+  //   slug: "metablock-technologies"
+  //   },
 
   // Add more case study objects as needed
 ];
