a2fc07933e26e95e534978d6e294f5fae7479a96
Naman Mittal <mittalnaman08@gmail.com>
2023-09-10 20:41:51 +0530

    Placement

--- src/components/Placement/Placement.jsx
+++ src/components/Placement/Placement.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import PlacementTimeLine from './PlacementTimeline'
+import Footer from '../Footer/Footer'
+import Header from '../Header/Header'
 
 const Placement = () => {
   return (
     <>
+        <Header />
         <PlacementTimeLine />
+        <div style={{marginTop: "40px"}}></div>
+        <Footer />
     </>
   )
 }
