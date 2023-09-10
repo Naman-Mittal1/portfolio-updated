cbd1f35d2fdd11eb0eb4b853a479142fdc9bd23c
Naman Mittal <mittalnaman08@gmail.com>
2023-09-10 17:37:53 +0530

    Interview Experience Added

--- src/data/interviewData.js
+++ src/data/interviewData.js
@@ -1,64 +1,128 @@
 const interviewData = [
     
+  {
+    id: 1,
+    title: "My Rollercoaster Interview Journey at a Cybersecurity Company",
+    description: "I recently had the opportunity to interview for a role at a cybersecurity company. While I wasn't ultimately selected for the position, it was a good learning experience...",
+    
+    tags: [
+      "CyberSecurity",
+      "College Placement",
+      "Interview",
+    ],
+
+    imageUrl: "/images/cybersecurity.jpg",
+
+
+    
+    overviewPara1: "I recently had the opportunity to interview for a role at a CyberSecurity Company. While I wasn't ultimately selected for the position, it was a good learning experience that taught me a lot about technical interviews and my own strengths and development areas.",
+    
+    overviewPara2: `The interview process spanned three rounds:`,
+
+    overviewPara3: `Aptitude Test - 60 multiple choice questions testing aptitude and coding to be solved in 60 minutes.`,
+    overviewPara4: `Technical Interview - Discussion of my BookMates project and data structures questions focused on arrays and linked lists.`,
+    overviewPara5: `Managerial Interview - Behavioral and HR questions about my background, skills, and fit.`,
+    overviewPara6: `Overall, I felt strong in the all the rounds, but i am disappointed to know that it didn't went my way. I don't know what went wrong during the last round, but I am hoping it was a bit of miscommunication which may have happened during the interview. It was a good opportunity!`,
+
+    
+    // resultHeading: "Result",
+
+    // resultPara: "The completed website effectively communicates Metablock's capabilities through a clean, modern design. Call-to-action buttons and contact forms generate leads. A news section and case studies establish thought leadership. Responsive design provides an optimal experience on mobile devices. The site aligned with brand guidelines and exceeded stakeholder expectations.",
+    
+    featuresPointHeading1: 'Resilience Amid Rejection',
+    featuresPointHeading2: 'Versatility and Stamina',
+    featuresPointHeading3: 'Communication Matters',
+    featuresPointHeading4: 'Invaluable Experiences',
+    featuresPointHeading5: 'Enhancing Self',
+
+    featuresPoint1: 'Rejection fuels inner strength to persevere.',
+    featuresPoint2: 'Multi-round interviews demand adaptability and unwavering resolve.',
+    featuresPoint3: 'Even with thorough technical preparation, effective communication is pivotal.',
+    featuresPoint4: 'The experience gained, relationship, and reflection is priceless.',
+    featuresPoint5: 'Seek to refine behavioral skills and self-confidence.',
+
+    featuresHeading: 'Key Outcomes',
+
+    interviewRound1: '-> Aptitude Test',
+    round1Content1: 'There were 60-minutes given as time limit that created intense pressure to answer 60 complex logic and coding problems. Those 60-minutes will be my fastest 60-minutes which felt like a few seconds and i love the thrill that i felt during that time.',
+    round1Content2: `I knew I could do this. I had been preparing for this moment for weeks. I had practiced solving logic and coding problems, and I had read up on the different types of questions that could be asked. I was ready. But as the clock ticked down, my anxiety started to get the best of me. I started to doubt myself. What if I wasn't good enough? What if I didn't get enough questions right?`,
+    round1Content3: 'I took a deep breath to calm down and clear my head. I was able to solve the first few problems relatively quickly, but then I started focusing on focusing on quality over quantity, as i was only solving those questions which i was sure about.',
+
+    interviewRound2: '-> Technical Interview',
+    round2Content1: 'I enjoyed geeking out over data structure concepts with the interviewer. It was great to talk to someone who was as passionate about technology as I was. We discussed my BookMates project and code samples. I felt like I was really connecting with him.',
+    round2Content2: `The linked list traversal and array manipulation questions were challenging, but I was able to answer them correctly. I struggled a bit with some of the optimizations, but the interviewer was patient and helpful. He gave me some great tips that I'll definitely use in the future.`,
+    round2Content3: 'There were a bit of logical and puzzle questions which were asked during the interview, and i was able to solve them quickly. It was my first time facing a technical interview, and i love that pressure which i felt, also loved how the interviewer was able to follow my thought process and ask clarifying questions.',
+
+    interviewRound3: '-> Managerial Interview',
+    round3Content1: 'This was the most personal interview of the day. We talked about my background, education, interests, and motivations. I shared my passion for cybersecurity  and my desire to grow my skills and the habit of learning and exploring different domains',
+    round3Content2: 'I also talked about my experience in working with teams and projects. I discussed the challenges I have faced and how I have overcome them. I talked about my passion for learning cybersecurity and how I want to proceed with it. We also talked about the company information and which role will suit me better. Also, we talked about my best & worst moments, the interviewers were friendly and it was a great experience.',
+    round3Content3: `I felt that the interview went well, but I was disappointed to learn that it didn't went my way. I am not sure what went wrong, but I am happy for the experience i have got during this process.`,
+
+    
+    conclusion: '',
+    // conclusionPara1: "The development of Metablock’s new corporate website was a success that aligned with their goals of building their brand, attracting clients, and generating business opportunities. As the lead front-end developer, I applied my skills in HTML5, CSS3, JavaScript, jQuery and responsive design to deliver a visually stunning, optimized, and fully-functional website. The pixel-perfect and accessible implementation along with robust performance resulted in an excellent user experience that conveyed Metablock’s blockchain expertise. Despite the tight timeline, the project was delivered on-schedule and within budget through agile practices and close collaboration with stakeholders. ",
+
+    // conclusionPara2: "The positive user feedback, increased traffic and higher lead conversion validated the quality of the end product. My technical skills and attention to detail were instrumental in building an impactful online presence for Metablock to establish thought leadership in their industry. This project provided invaluable experience and I am proud to have contributed to the company’s vision coming to life through their new website.",
+
+    // lastInfo: "Checkout the website: ",
+
+    // exploreLink: "https://metablocktechnologies.org/",
+
+
+    slug: "cybersecurity-company"
+    },
     {
-      id: 1,
-      title: "My Rollercoaster Interview Journey at a Cybersecurity Company",
-      description: "I recently had the opportunity to interview for a role at a cybersecurity company. While I wasn't ultimately selected for the position, it was a good learning experience...",
+      id: 2,
+      title: "My Interview Experience at a Service-Based Company",
+      description: "I recently interviewed at a company, which work around every domain, from web to app and from devops to ML. It was an disappointing experience...",
       
       tags: [
-        "CyberSecurity",
+        "Service-Based Company",
         "College Placement",
         "Interview",
       ],
   
-      imageUrl: "/images/cybersecurity.jpg",
+      imageUrl: "/images/service-based.png",
   
   
       
-      overviewPara1: "I recently had the opportunity to interview for a role at a CyberSecurity Company. While I wasn't ultimately selected for the position, it was a good learning experience that taught me a lot about technical interviews and my own strengths and development areas.",
+      overviewPara1: "I recently had the opportunity to interview for a role at a Service-Based Company. I wasn't ultimately selected for the position  and no specific reason was provided. It was a valuable learning experience that showed technical knowledge alone is not enough, having luck is a necessity.",
       
-      overviewPara2: `The interview process spanned three rounds:`,
+      overviewPara2: `The interview process spanned two rounds:`,
 
-      overviewPara3: `Aptitude Test - 60 multiple choice questions testing aptitude and coding to be solved in 60 minutes.`,
-      overviewPara4: `Technical Interview - Discussion of my BookMates project and data structures questions focused on arrays and linked lists.`,
-      overviewPara5: `Managerial Interview - Behavioral and HR questions about my background, skills, and fit.`,
-      overviewPara6: `Overall, I felt strong in the all the rounds, but i am disappointed to know that it didn't went my way. I don't know what went wrong during the last round, but I am hoping it was a bit of miscommunication which may have happened during the interview. It was a good opportunity!`,
+      overviewPara3: `Coding Test - One coding question to be solved in 90 minutes. The array/vector question was of medium to hard level difficulty. Only a few candidates were able to solve it fully.`,
+      overviewPara4: `Technical + HR Interview - Discussed my projects, hackathons, internships, and were asked about coding round logic. Also asked React, Node.js, APIs, SQL vs MongoDB, 2-3 medium coding questions to solve and provide pseudocode for. Discussed time complexities of pseudocode solutions.`,
+      overviewPara5: `Overall it was an intense interview process that tested both my technical and communication skills. While disappointed to not make it through even after answering all the questions correctly and giving my best effort  .`,
   
       
       // resultHeading: "Result",
   
       // resultPara: "The completed website effectively communicates Metablock's capabilities through a clean, modern design. Call-to-action buttons and contact forms generate leads. A news section and case studies establish thought leadership. Responsive design provides an optimal experience on mobile devices. The site aligned with brand guidelines and exceeded stakeholder expectations.",
       
-      featuresPointHeading1: 'Resilience Amid Rejection',
-      featuresPointHeading2: 'Versatility and Stamina',
-      featuresPointHeading3: 'Communication Matters',
-      featuresPointHeading4: 'Invaluable Experiences',
-      featuresPointHeading5: 'Enhancing Self',
+      featuresPointHeading1: 'Tested Technical Skills',
+      featuresPointHeading2: 'Communication Skills',
+      featuresPointHeading3: 'Valuable Feedback',
+      featuresPointHeading4: 'SQL is Important',
+      featuresPointHeading5: 'Always be Confident',
   
-      featuresPoint1: 'Rejection fuels inner strength to persevere.',
-      featuresPoint2: 'Multi-round interviews demand adaptability and unwavering resolve.',
-      featuresPoint3: 'Even with thorough technical preparation, effective communication is pivotal.',
-      featuresPoint4: 'The experience gained, relationship, and reflection is priceless.',
-      featuresPoint5: 'Seek to refine behavioral skills and self-confidence.',
+      featuresPoint1: 'The coding rounds stretched my data structures, algorithms, and optimization skills.',
+      featuresPoint2: 'Explaining my thought process clearly was just as important as solving problems correctly.',
+      featuresPoint3: 'The specific feedback on areas to improve will help me continue enhancing my skills',
+      featuresPoint4: `Doesn't matter either you mention it in your resume or not, interviewer will just ask it.`,
+      featuresPoint5: 'Always know your worth, and confidence gives you an extra edge.',
   
       featuresHeading: 'Key Outcomes',
   
-      interviewRound1: '-> Aptitude Test',
-      round1Content1: 'There were 60-minutes given as time limit that created intense pressure to answer 60 complex logic and coding problems. Those 60-minutes will be my fastest 60-minutes which felt like a few seconds and i love the thrill that i felt during that time.',
-      round1Content2: `I knew I could do this. I had been preparing for this moment for weeks. I had practiced solving logic and coding problems, and I had read up on the different types of questions that could be asked. I was ready. But as the clock ticked down, my anxiety started to get the best of me. I started to doubt myself. What if I wasn't good enough? What if I didn't get enough questions right?`,
-      round1Content3: 'I took a deep breath to calm down and clear my head. I was able to solve the first few problems relatively quickly, but then I started focusing on focusing on quality over quantity, as i was only solving those questions which i was sure about.',
+      interviewRound1: '-> Coding Round',
+      round1Content1: 'The 90 minute time limit added tremendous pressure to solve a complex array/vector problem. It tested both analytical thinking and coding skills under tight constraints. I was determined to do my best.',
+      round1Content2: `Practicing Striver's Sheet and other coding questions helped me feel prepared for the coding round. But as time ticked down, I focused on finding the logic rather than rushed coding.  I spent the first 20 minutes analyzing test cases and figuring out the logic.`,
+      round1Content3: 'Once i understood the logic, it just took me 10 mins to code the complete solution. It is important to consider all possible cases before submitting code. I tried optimizing complexity wherever possible.',
 
-      interviewRound2: '-> Technical Interview',
-      round2Content1: 'I enjoyed geeking out over data structure concepts with the interviewer. It was great to talk to someone who was as passionate about technology as I was. We discussed my BookMates project and code samples. I felt like I was really connecting with him.',
-      round2Content2: `The linked list traversal and array manipulation questions were challenging, but I was able to answer them correctly. I struggled a bit with some of the optimizations, but the interviewer was patient and helpful. He gave me some great tips that I'll definitely use in the future.`,
-      round2Content3: 'There were a bit of logical and puzzle questions which were asked during the interview, and i was able to solve them quickly. It was my first time facing a technical interview, and i love that pressure which i felt, also loved how the interviewer was able to follow my thought process and ask clarifying questions.',
+      interviewRound2: '-> Technical + HR Interview',
+      round2Content1: `As Always, i enjoyed discussing about my BookMates project, it feels so good to get the feedback from different people and also promoting it to them. I enjoyed discussing about React, Node.js and other technologies I'm passionate about. It was great interacting with insightful engineers who appreciated my work.`,
+      round2Content2: `The data structures and algorithms questions were challenging. I got most right, but struggled with some optimizations. The interviewer was trying to ask the hard question to get me down, but i already practiced enough problem to answer all the questions given to me.`,
+      round2Content3: 'Even though i struggled in solving the SQL query as i have not practiced them much, and did not even mentioned in my resume, but the interviewer was keen to ask SQL queries. Communication was very important there. I focused on explaining my thought process clearly while solving problems.',
 
-      interviewRound3: '-> Managerial Interview',
-      round3Content1: 'This was the most personal interview of the day. We talked about my background, education, interests, and motivations. I shared my passion for cybersecurity  and my desire to grow my skills and the habit of learning and exploring different domains',
-      round3Content2: 'I also talked about my experience in working with teams and projects. I discussed the challenges I have faced and how I have overcome them. I talked about my passion for learning cybersecurity and how I want to proceed with it. We also talked about the company information and which role will suit me better. Also, we talked about my best & worst moments, the interviewers were friendly and it was a great experience.',
-      round3Content3: `I felt that the interview went well, but I was disappointed to learn that it didn't went my way. I am not sure what went wrong, but I am happy for the experience i have got during this process.`,
-  
-      
       conclusion: '',
       // conclusionPara1: "The development of Metablock’s new corporate website was a success that aligned with their goals of building their brand, attracting clients, and generating business opportunities. As the lead front-end developer, I applied my skills in HTML5, CSS3, JavaScript, jQuery and responsive design to deliver a visually stunning, optimized, and fully-functional website. The pixel-perfect and accessible implementation along with robust performance resulted in an excellent user experience that conveyed Metablock’s blockchain expertise. Despite the tight timeline, the project was delivered on-schedule and within budget through agile practices and close collaboration with stakeholders. ",
   
@@ -69,10 +133,8 @@ const interviewData = [
       // exploreLink: "https://metablocktechnologies.org/",
   
   
-      slug: "cybersecurity-company"
+      slug: "service-based-company"
       },
+];
 
-  ];
-  
-  export default interviewData;
-  
+export default interviewData;
